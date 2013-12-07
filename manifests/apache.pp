b2962df3702caf70b628d4107495628fff0d7d32
Niko Kiuru <niko@kiuru.net>
2013-12-07 16:09:27 +0200

    Install apache2

---
+++ manifests/apache.pp
@@ -0,0 +1,13 @@
+class rails::apache {
+
+	package {"apache2":
+                ensure => present,
+        }
+
+        service {"apache2":
+                ensure => running,
+                enable => true,
+                require => Package["apache2"],
+        }	
+
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
-class puppet-rails-passenger {
-  file { '/tmp/helloPuppet':
-    content => "Hello Puppet!"
-  }
+class rails {
+
+	include rails::apache
+
 }
