8735b4abb20cc0669a4ff6149e0f03b4ecb0d474
Niko Kiuru <niko@kiuru.net>
2013-12-12 19:21:05 +0200

    Add rvm class dependencies

--- manifests/rvm.pp
+++ manifests/rvm.pp
@@ -1,8 +1,13 @@
 class rails::rvm {
 
+	package {"curl":
+		ensure => present,
+	}
+
 	exec {"install-rvm":
 		command => "curl -L https://get.rvm.io | bash -s stable --ruby --autolibs=enable --auto-dotfiles",
 		path    => "/usr/local/bin/:/usr/bin/:/bin/",
+		require => Package["curl"],
 	}
 
 }
