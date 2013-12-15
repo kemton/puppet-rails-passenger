6eac2dc1de414f7a13406c2f2b5990ec1826d0eb
Niko Kiuru <niko@kiuru.net>
2013-12-15 18:18:31 +0200

    rvm installing rails, and made rvm working for all users

--- manifests/rvm.pp
+++ manifests/rvm.pp
@@ -5,10 +5,9 @@ class rails::rvm {
 	}
 
 	exec {"install-rvm":
-		command => "curl -L https://get.rvm.io | bash -s stable --ruby --autolibs=enable --auto-dotfiles",
+		command => "curl -L https://get.rvm.io | sudo bash -s stable --rails --autolibs=enable --auto-dotfiles",
 		path    => "/usr/local/bin:/usr/bin:/usr/sbin:/bin",
 		require => Package["curl"],
-		creates => "/usr/local/rvm/bin/rvm",
 	}
 
 }
