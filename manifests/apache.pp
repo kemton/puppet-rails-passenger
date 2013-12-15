7c89ba919918126482625eb9d693178fcde1136d
Niko Kiuru <niko@kiuru.net>
2013-12-16 00:37:39 +0200

    Defined types Apache VirtualHost

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -10,4 +10,25 @@ class apache {
 			require => Package["apache2"],
 	}	
 
+	apache::vhost { "hello-rails":
+		document_root => "/home/rails/public_html/hello-rails/public",
+	}
+
 }
+
+define apache::vhost(
+	$port          = "80",
+	$server_name   = $title,
+	$document_root = "/var/www/${server_name}",
+	) {
+
+	include apache
+
+	file { "/etc/apache2/sites-enabled/${server_name}.conf":
+		content => template("rails/passenger-vhost.conf.erb"),
+		owner   => "root",
+		group   => "root",
+		mode    => "755",
+		require => Package["apache2"],
+		notify  => Service["apache2"],
+	}
